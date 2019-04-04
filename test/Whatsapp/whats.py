c1b69dedb52c91b94f1260bb6be7b9d20d688cf4
Humberto_Santana <hsantana.2611@gmail.com>
2019-04-04 02:19:41 -0600

    Agregué el código con el que se pueden mandar whatsapp desde python3

---
+++ test/Whatsapp/whats.py
@@ -0,0 +1,14 @@
+from twilio.rest import Client
+
+account_sid = 'ACb149282acccbdf97c1004710c2e3d11d'
+auth_token = '8d0aa7e211ea99f907ea7b6de8f4ca46'
+
+cliente = Client(account_sid, auth_token)
+
+mensaje = cliente.messages.create(
+    body = 'Ya hay whatsapp !!',
+    from_= 'whatsapp:+14155238886',
+    to = 'whatsapp:+5215545077393'
+)
+
+print(mensaje.sid)
