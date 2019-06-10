b0c0eb85a06d0fa74380e1dd4747c006efb52bd9
Humberto Santana <hsantana.2611@gmail.com>
2019-06-10 15:45:11 -0500

    Correcion de la pregunta 1 tabla 6

--- test/Tabla6/Pregunta1/pregunta1.py
+++ test/Tabla6/Pregunta1/pregunta1.py
@@ -1,4 +1,8 @@
-""" PREGUNTA 1 - TABLA 6 - REDES III """
+""" PREGUNTA 1 - TABLA 6 """
 from funcionesP1 import *
+direcciones = ['10.1.200.1', '10.3.200.2', '10.4.200.2', '10.6.200.2', '10.8.200.2', '10.14.200.1', '10.9.200.2', '10.10.200.1']
 
-vincularDatos() 
+for direccion in direcciones:
+    nombre = extraerNombre(direccion) 
+    extraerInformacion(nombre, direccion)
+    tratarInformacion(nombre)
